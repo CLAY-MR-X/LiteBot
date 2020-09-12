8a486277f988554c66242c81cb0223e4035a558a
DarkGa <48998251+DarkGa@users.noreply.github.com>
2020-09-12 09:41:41 +0300

    Обновлено для новой версии pyrogram

--- helper.py
+++ helper.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, Filters
+from pyrogram import Client, filters
 import os
 import subprocess
 from io import StringIO
@@ -14,16 +14,15 @@ you_id=Ваш айди аккаунта
 app = Client("name", 
                       api_id=Ваш апи айди,
                       api_hash="ваш апи хеш")
-@app.on_message(Filters.text)
+@app.on_message(filters.text, filters.me)
 def hello(client, m):
-	if m.from_user.id==you_id:
-		if "py3" in m.text.lower():
-			script=m.text.replace("py3 ", "")
-			old_stdout = sys.stdout
-			result = sys.stdout = StringIO()
-			exec(script)
-			sys.stdout = old_stdout 
-			m.edit(f'''**code**:
+	if "py3" in m.text.lower():
+		script=m.text.replace("py3 ", "")
+		old_stdout = sys.stdout
+		result = sys.stdout = StringIO()
+		exec(script)
+		sys.stdout = old_stdout 
+		m.edit(f'''**code**:
 	
 ```{script}```
 	
@@ -31,11 +30,11 @@ def hello(client, m):
 	
 ```{result.getvalue()}```
 ''')
-		if "bash" in m.text.lower():
-			test1=m.text.replace("bash ", "")
-			script=test1.format(m, app)
-			test=os.popen(f'''{script}''').read()
-			m.edit(f'''**code**:
+	if "bash" in m.text.lower():
+		test1=m.text.replace("bash ", "")
+		script=test1.format(m, app)
+		test=os.popen(f'''{script}''').read()
+		m.edit(f'''**code**:
 	
 ```{script}```
 	
@@ -43,25 +42,25 @@ def hello(client, m):
 	
 ```{test}```
 	''')
-		if 'bind ' in  m.text:
-			reply=m.reply_to_message
-			params=m.text.replace('bind ', '')
-			try:
-				save=open('binds/'+params+'.py', 'w')
-				save.write(reply.text)
-				save.close()
-				m.edit('**Бинд успешно настроен!**')
-			except Exception as e:
-				print(e)
+	if 'bind ' in  m.text:
+		reply=m.reply_to_message
+		params=m.text.replace('bind ', '')
+		try:
+			save=open('binds/'+params+'.py', 'w')
+			save.write(reply.text)
+			save.close()
+			m.edit('**Бинд успешно настроен!**')
+		except Exception as e:
+			print(e)
 			
-		else:
-			files=os.listdir('binds')
-			for file in files:
-				if file.replace('.py', '') in m.text[:len(file.replace('.py', ''))]:
-					ok=open('binds/'+file, 'r')
-					script=ok.read()
-					reply=m.reply_to_message
-					exec(script)
-					ok.close()
+	else:
+		files=os.listdir('binds')
+		for file in files:
+			if file.replace('.py', '') in m.text[:len(file.replace('.py', ''))]:
+				ok=open('binds/'+file, 'r')
+				script=ok.read()
+				reply=m.reply_to_message
+				exec(script)
+				ok.close()
 					
 app.run()
